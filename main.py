00458ea199b2940c30e681a2492adda167371f8a
nTerx <nterx22@gmail.com>
2026-04-08 06:55:15 +0700

    append amount and description to the list of ledger and create object food

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ class Category:
             'amount': amount,
             'description' : description
         }
+        self.ledger.append(form)
+    
+food = Category('Food')
+food.deposit(1000, 'initial deposit')
+# create object in the ledger instance variable
