f76d224beb1a9ac1cbf99416bc532592c1d5cdee
nTerx <nterx22@gmail.com>
2026-04-09 07:49:57 +0700

    Create check_funds method and fix withdraw method by add if statement check_funds before withdraw successful

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ class Category:
             'amount' : -amount,
             'description': description
         }
-        self.ledger.append(form)
+        if self.check_funds(amount):
+            self.ledger.append(form)
+            return True
+        return False
     
     def get_balance(self):
         return sum(item['amount'] for item in self.ledger)
@@ -23,6 +26,12 @@ class Category:
     def transfer(self, amount, destination):
         self.withdraw(amount, f"Transfer to {destination.name}")
         destination.doposit(amount, f"Transfer from {self.name}")
+        
+    def check_funds(self, amount):
+        if self.get_balance() >= amount:
+            return True
+        else:
+            return False
         
 food = Category('Food')
 food.deposit(1000, 'initial deposit')
