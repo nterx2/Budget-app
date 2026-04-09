fd927915341af311871f4aec80b72c2fd8898b56
nTerx <nterx22@gmail.com>
2026-04-09 07:42:05 +0700

    create get balance method for return sum amount

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ class Category:
             'description': description
         }
         self.ledger.append(form)
+    
+    def get_balance(self):
+        return sum(item['amount'] for item in self.ledger)
         
 food = Category('Food')
 food.deposit(1000, 'initial deposit')
